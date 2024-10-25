67c7a379e93e4590d9b5b613ddf38367ed57b1b0
Marlianti01 <156512142+Marlianti01@users.noreply.github.com>
2024-10-25 23:48:32 +0800

    lab1 for native programming

---
+++ LAB1 NATIVE/Lab1/task1-kotlin/app/src/main/java/com/example/myfirstappkotlin/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myfirstappkotlin
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var button:Button
+        var textView: TextView
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        textView = findViewById(R.id.textView)
+        textView.setTextColor(Color.RED)
+        button = findViewById(R.id.button)
+        button.setOnClickListener{
+            textView.text="Hello"
+        }
+
+
+    }
+
+}
